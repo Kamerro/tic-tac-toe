63a34a62af9dab8472756be486fde40831c069d4
Kamil Mikuła <98419600+Kamerro@users.noreply.github.com>
2022-12-18 16:18:27 +0100

    Add files via upload

---
+++ Kolko_Krzyzyk/Kolko_Krzyzyk/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Kolko_Krzyzyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.kto_ma_ruch = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(25, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 380);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kto ma teraz ruch?";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(137, 27);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(250, 27);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(25, 141);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(137, 141);
+            this.button5.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Location = new System.Drawing.Point(249, 141);
+            this.button6.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(25, 255);
+            this.button7.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Location = new System.Drawing.Point(137, 255);
+            this.button8.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button9.Location = new System.Drawing.Point(249, 255);
+            this.button9.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // kto_ma_ruch
+            // 
+            this.kto_ma_ruch.AutoSize = true;
+            this.kto_ma_ruch.Location = new System.Drawing.Point(262, 380);
+            this.kto_ma_ruch.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.kto_ma_ruch.Name = "kto_ma_ruch";
+            this.kto_ma_ruch.Size = new System.Drawing.Size(0, 29);
+            this.kto_ma_ruch.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(365, 451);
+            this.Controls.Add(this.kto_ma_ruch);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label kto_ma_ruch;
+    }
+}
+
---
+++ Kolko_Krzyzyk/Kolko_Krzyzyk/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolko_Krzyzyk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            kto_ma_ruch.Text = "O";
+        }
+        //jeżeli wartosc jest true, to ruch kółka
+        //else ruch krzyżyka
+        bool czyj_ruch = true;
+        private void WstawZnak(object przycisk)
+        {
+            string napis = "";
+            //Ustawiamy referencję na przycisk
+            Button wcisnietyPrzycisk = (Button)przycisk;
+            //na podstawie kogo jest ruch
+            if (czyj_ruch == true)
+            {
+                wcisnietyPrzycisk.Text = "O";
+                kto_ma_ruch.Text = "X";
+            }
+            else
+            {
+                wcisnietyPrzycisk.Text = "X";
+                kto_ma_ruch.Text = "O";
+            }
+            wcisnietyPrzycisk.Enabled = false;
+            bool czy_ktos_wygral = SprawdzCzyKtosWygral();
+
+            if (czy_ktos_wygral == true)
+            {
+                if (czyj_ruch)
+                {
+                    napis = "Wygrało kółko";
+                }
+                else
+                {
+                    napis = "Wygrał krzyżyk";
+                }
+
+                DialogResult odp = MessageBox.Show(napis + ". Czy chcesz rozpocząć ponownie?", "Koniec", MessageBoxButtons.YesNo);
+                if (odp == DialogResult.Yes)
+                {
+                    ResetPlanszy();
+                }
+                else
+                    Koniec();
+
+
+            }
+            else
+            {
+                //sprawdzamy czy ktoś wygrał
+                czyj_ruch = !czyj_ruch;
+            }
+        }
+        private void ResetPlanszy()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            czyj_ruch = true;
+            kto_ma_ruch.Text = "O";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+
+        private void Koniec()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+        }
+
+        private bool SprawdzCzyKtosWygral()
+        {
+            // sprawdzanie wierszy
+            //sprawdzamy warunek dla pierwszego wiersza
+            if(button1.Text == button2.Text && button2.Text == button3.Text && button1.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+
+            // sprawdzanie kolumn
+
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && button2.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+
+            //Przekątne
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Text != "")
+            {
+                //zwracamy prawdę jeżeli pierwszy wiersz ma 3 kółka lub 3 krzyżyki
+                return true;
+            }
+
+            return false;
+            // sprawdzanie skosów
+        }
+
+        //sendery
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            WstawZnak(sender);
+        }
+    }
+}
